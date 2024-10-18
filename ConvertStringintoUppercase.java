ac6ce2ae4e1cd98a7849721f7eda9b2e268935f1
mohini1103 <mohiniddeshpande@gmail.com>
2024-10-18 09:42:47 +0530

    Add files via upload

---
+++ ConvertStringintoUppercase.java
@@ -0,0 +1,11 @@
+package com.jbk.oct14;
+
+public class ConvertStringintoUppercase {
+
+	public static void main(String[] args) {
+
+		StringBuffer s1 = new StringBuffer("Hello welcome to JBK");
+
+		System.out.println(s1.toString().toUpperCase());
+	}
+}
